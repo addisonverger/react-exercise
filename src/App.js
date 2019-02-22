9040b81d52bd3631911f5d4d84be65b114fb9c13
Addison Verger <addison.j.verger@gmail.com>
2019-02-22 14:38:48 -0600

    Lorem-Ipsum complete

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import LoremIpsum from './Lorem-Ipsum.js'
+import HelloReact from './Hello-React.js'
 
-//split helloreact into seperate file like lorem-ipsum
 
-const HelloReact = () => {
-  return (
-    <section className="content">
-      <h1 className="title">Hello React!</h1>
-      <p>React makes it painless to create interactive UIs. Design simple views
-      for each state in your application, and React will efficiently update and
-      render just the right components when your data changes.</p>
-      <p>Declarative views make your code more predictable and easier to debug.</p>
-    </section>
-  )
-}
 
 const LoginForm = () => {
   return (
@@ -37,7 +26,7 @@ class App extends Component {
   }
   render() {
     const contents = {
-      'hello-react': HelloReact,
+      'hello-react': <HelloReact />,
       'lorem-ipsum': <LoremIpsum />,
       'login-form': LoginForm,
     }
--- src/Lorem-Ipsum.js
+++ src/Lorem-Ipsum.js
@@ -1,30 +1,54 @@
 import React, { Component } from 'react';
 
-//update state for addParagraph and deleteParagraph
+let loremIpsumParagraph = `Lorem ipsum dolor sit amet, consectetur adipiscing
+  elit. Vivamus faucibus at magna sit amet tristique. Suspendisse ut varius dui,
+  tincidunt semper sapien. Nullam bibendum eros lectus, eu posuere velit
+  ultrices sed. Proin consectetur lacus nec metus ullamcorper, ac tempus
+  felis eleifend. Donec eu euismod nisl. Morbi fringilla justo sit amet
+  blandit dictum. Fusce sollicitudin ipsum ut mauris posuere pharetra.
+  Praesent vitae elit nec ex placerat faucibus a in diam. Sed bibendum sit
+  amet dui at facilisis. Vivamus vitae felis lacinia, gravida lectus id,
+  placerat ipsum. Interdum et malesuada fames ac ante ipsum primis in
+  faucibus. Integer feugiat, sem in interdum tempor, nisi enim mollis nibh,
+  luctus laoreet massa ligula nec ex.`
 
 class LoremIpsum extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      paragraphArray: []
+      paragraphArray: [loremIpsumParagraph, loremIpsumParagraph]
     }
   }
   addParagraph = () => {
-
+    var newArray = this.state.paragraphArray
+    newArray.push(loremIpsumParagraph)
+    this.setState({
+      paragraphArray: newArray
+    })
+  }
+  removeParagraph = () => {
+    var newArray = this.state.paragraphArray
+    newArray.pop()
+    this.setState({
+      paragraphArray: newArray
+    })
   }
   render() {
     return (
       <section className="content">
         <h1 className="title">
-          "Number of Lorem Ipsum paragraphs:"
-          {this.state.paragraphArray.length}
+          Number of Lorem Ipsum paragraphs: {this.state.paragraphArray.length}
         </h1>
         <div className="buttons">
           <button className="button is-info is-medium" onClick={this.addParagraph}>Add Paragraph</button>
-          <button className="button is-info is-medium">Remove Paragraph</button>
+          <button className="button is-info is-medium" onClick={this.removeParagraph}>Remove Paragraph</button>
         </div>
         <div className="content">
-          {/* {Paragraphs} */}
+          {
+            this.state.paragraphArray.map((paragraph, index) => {
+              return (<p key={index}>{paragraph}</p>)
+            })
+          }
         </div>
       </section>
     )
